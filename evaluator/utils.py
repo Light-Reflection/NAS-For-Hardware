6127dd8dcf71b1e2c6ab4981a83662e1819224ee
yuanliuchun <yuanliuchun@dm-ai.cn>
2019-10-19 20:20:41 +0800

    #4 update cifar transform

--- evaluator/utils.py
+++ evaluator/utils.py
@@ -105,7 +105,7 @@ def cifar10_data_transform():
         transforms.RandomCrop(32, padding=4),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
-        # transforms.Normalize(cfg['data_mean'], cfg['data_std']),
+        transforms.Normalize([0.49139968, 0.48215827, 0.44653124], [0.24703233, 0.24348505, 0.26158768]),
         ])
 
     # if cfg['cutout']:
@@ -113,7 +113,7 @@ def cifar10_data_transform():
 
     valid_transform = transforms.Compose([
         transforms.ToTensor(),
-        # transforms.Normalize(cfg['data_mean'], cfg['data_std']),
+        transforms.Normalize([0.49139968, 0.48215827, 0.44653124], [0.24703233, 0.24348505, 0.26158768]),
         ])
     return train_transform, valid_transform
 
