2f3de0535e3a389d774bed0e490e48a026c0e074
yuanliuchun <yuanliuchun@dm-ai.cn>
2019-10-31 17:14:31 +0800

    #9 fix the rewrite writers and seprate the trainS and trainD

--- generator/automodel.py
+++ generator/automodel.py
@@ -29,7 +29,7 @@ class AutoModel(object):
             model = None,
             train_data_loader = None, 
             valid_data_loader = None,
-            max_samples = 1000000,
+            max_samples = 100,
             target_acc = 100,
             top_k = 5,
             batch_size=128,
@@ -78,7 +78,7 @@ class AutoModel(object):
             valid_queue = valid_data_loader
 
             self._trainer = Trainer(model, train_queue, valid_queue, epoch, optimizer, scheduler, criterion, logger, writer, rank, world_size) # init trainer
-            self._trainer.run(save_path=os.path.join(save_path, 'checkpoints'))
+            self._trainer.run(save_path=os.path.join(save_path, 'SuperNet-checkpoints'))
 
         self.search(model,top_k,target_acc,max_samples)
         ite = 0
@@ -90,10 +90,9 @@ class AutoModel(object):
             model.dispatch(resolution_encoding,channel_encoding, op_encoding)
         for j in range(len(self.subnets)):
             train_queue, valid_queue = load_data(data_root, batch_size=128, num_workers=4)
-            self._trainer = Trainer(self.subnets[j], train_queue, valid_queue, epoch, optimizer, scheduler, criterion, logger, writer, rank, world_size)
-            self.submodelname = ''
-            self.submodelname += '-submodel-number-'
-            self.submodelname += str(j)
+            self._trainer = Trainer(self.subnets[j], train_queue, valid_queue, epoch, optimizer, scheduler, criterion, logger, writer, rank, world_size, stats='D'+str(j))
+            # Train DispatchNet 
+            self.submodelname = 'submodel-number-{}-checkpoints'.format(j)
             self._trainer.run(save_path = os.path.join(save_path, self.submodelname))
             print('*'*20,'Subnet number',ite,'training finished!','*'*20)
 
